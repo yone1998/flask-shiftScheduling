a9474ef583ee3b297aab8d3ba45f6ad2ace45149
yonetashoichi <shoichi.spb@icloud.com>
2022-09-05 22:51:44 +0900

    Delete hopeDay.js

--- static/js/hopeDay.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('demo').innerHTML = 'this is javascript'
-
-var btn = document.getElementById('selectDay15');
-console.log('check1')
-btn.addEventListener('click', function() {
-    console.log('click')
-    btn.style.backgroundColor = 'pink';
-});
